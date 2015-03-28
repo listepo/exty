769b7e47096b916d5fad39fb62ab9956e2687325
Ivan <listepo@ya.ru>
2015-03-28 03:36:36 +0200

    Add beautify for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var beautify = require('gulp-jsbeautify');
 
 gulp.task('lint', function() {
   return gulp.src('./**.js')
@@ -13,4 +14,9 @@ gulp.task('lint', function() {
 gulp.task('test', function () {
     return gulp.src('./test/**.js', {read: false})
         .pipe(mocha());
+});
+
+gulp.task('beautify', function() {
+  gulp.src('./**.js')
+    .pipe(beautify());
 });
